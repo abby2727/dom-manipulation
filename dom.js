0dabeac40364ad4d4c237c6da8f1aec64d228d95
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-20 11:35:07 +0800

    Update dom.js

--- dom.js
+++ dom.js
@@ -6,9 +6,9 @@ items[2].style.fontWeight = 'bold';
 items[2].style.color = 'red';
 items[0].style.color = 'orange';
 
-for (let i = 0; i < items.length; i++) {
-    items[i].style.backgroundColor = 'lightgray';
-}
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.color = 'lightgray';
+// }
 
 let myInput = document.querySelector('input');
 myInput.style.backgroundColor = '#000';
@@ -23,3 +23,8 @@ listItemLast.style.color = 'yellow';
 
 let listItemSecond = document.querySelector('.list-group-item:nth-child(2)');
 listItemSecond.style.color = 'blue';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'lightGray';
+}
