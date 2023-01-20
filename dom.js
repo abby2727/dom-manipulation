7cfc6d36741e7525541877cca9e67a4a73e41f75
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-20 11:00:41 +0800

    First commit

---
+++ dom.js
@@ -0,0 +1,26 @@
+// getElementsByClassName use when woring with array elements
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+// items[2].textContent = 'Hi ITEM 3';
+items[2].style.fontWeight = 'bold';
+items[2].style.color = 'red';
+items[0].style.color = 'orange';
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = 'lightgray';
+}
+
+let myInput = document.querySelector('input');
+myInput.style.backgroundColor = '#000';
+myInput.value = 'Hello ITEM';
+myInput.style.color = '#FFFFFF';
+
+let myButton = document.querySelector('input[type=submit]');
+myButton.value = 'Send';
+
+let listItemLast = document.querySelector('.list-group-item:last-child');
+listItemLast.style.color = 'yellow';
+
+let listItemSecond = document.querySelector('.list-group-item:nth-child(2)');
+listItemSecond.style.color = 'blue';
