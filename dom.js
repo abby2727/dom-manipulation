c90601dc73aa3f05d3cd676d6f617f6c1a6a6a22
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-24 22:46:27 +0800

    Update

--- dom.js
+++ dom.js
@@ -1,30 +1,70 @@
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
 
-// items[2].textContent = 'Hi ITEM 3';
-items[2].style.fontWeight = 'bold';
-items[2].style.color = 'red';
-items[0].style.color = 'orange';
+// items[2].style.fontWeight = 'bold';
+// items[2].style.color = 'red';
+// items[0].style.color = 'orange';
 
-// for (let i = 0; i < items.length; i++) {
-//     items[i].style.color = 'lightgray';
+// let myInput = document.querySelector('input');
+// myInput.style.backgroundColor = '#000';
+// myInput.value = 'Hello ITEM';
+// myInput.style.color = '#FFFFFF';
+
+// let myButton = document.querySelector('input[type=submit]');
+// myButton.value = 'Send';
+
+// let listItemLast = document.querySelector('.list-group-item:last-child');
+// listItemLast.style.color = 'yellow';
+
+// let listItemSecond = document.querySelector('.list-group-item:nth-child(2)');
+// listItemSecond.style.color = 'blue';
+
+// let odd = document.querySelectorAll('li:nth-child(even)');
+// for (let i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = 'lightGray';
 // }
 
-let myInput = document.querySelector('input');
-myInput.style.backgroundColor = '#000';
-myInput.value = 'Hello ITEM';
-myInput.style.color = '#FFFFFF';
+// * Traversing the DOM
 
-let myButton = document.querySelector('input[type=submit]');
-myButton.value = 'Send';
+// let itemList = document.getElementById("items");
 
-let listItemLast = document.querySelector('.list-group-item:last-child');
-listItemLast.style.color = 'yellow';
+// console.log(itemList.lastElementChild);
+// itemList.firstElementChild.style.color = 'red';
 
-let listItemSecond = document.querySelector('.list-group-item:nth-child(2)');
-listItemSecond.style.color = 'blue';
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'lightGray';
 
-let odd = document.querySelectorAll('li:nth-child(even)');
-for (let i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = 'lightGray';
-}
+// console.log(itemList.children);
+// itemList.children[0].style.backgroundColor = 'blue';
+// itemList.children[1].style.backgroundColor = 'blue';
+// itemList.children[2].style.backgroundColor = 'blue';
+// itemList.children[3].style.backgroundColor = 'blue';
+// for (let i = 0; i < itemList.children.length; i++) {
+//     itemList.children[i].style.backgroundColor = 'green';
+// }
+// Array.prototype.forEach.call(itemList.children, function(item) {
+//     item.style.backgroundColor = 'green';
+// });
+// for (const child of itemList.children) {
+//     child.style.backgroundColor = 'blue';
+// }
+
+// * Create Element 
+
+let div = document.createElement('div');
+
+div.id = 'myDivId';
+
+// Create text node
+let divTxt = document.createTextNode('Hello, world!');
+div.appendChild(divTxt);
+
+let container = document.querySelector('.container #main');
+let h2 = document.querySelector('#main h2');
+
+// console.log(div);
+container.insertBefore(div, h2);
+
+let changeMe = document.getElementById('myDivId');
+// console.log(changeMe);
+changeMe.style.backgroundColor = 'blue';
