34d098d21b2ad2090cb8bddc24c3c4483b826fd0
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-27 16:41:04 +0800

    Update

--- sample.js
+++ sample.js
@@ -30,48 +30,74 @@
 // * Part 2
 // * Traversing the DOM
 
-// let itemList = document.getElementById("items");
+// let itemList = document.querySelector('#items');
 
-// console.log(itemList.lastElementChild);
-// itemList.firstElementChild.style.color = 'red';
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'blue';
+// console.log(itemList.parentNode.parentNode);
 
+// parentElement
 // console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = 'lightGray';
+// itemList.parentElement.style.backgroundColor = 'red';
+// console.log(itemList.parentElement.parentElement);
 
+// childNode
+// console.log(itemList.childNodes);
+
+// children
 // console.log(itemList.children);
-// itemList.children[0].style.backgroundColor = 'blue';
-// itemList.children[1].style.backgroundColor = 'blue';
-// itemList.children[2].style.backgroundColor = 'blue';
-// itemList.children[3].style.backgroundColor = 'blue';
-// for (let i = 0; i < itemList.children.length; i++) {
-//     itemList.children[i].style.backgroundColor = 'green';
-// }
-// Array.prototype.forEach.call(itemList.children, function(item) {
-//     item.style.backgroundColor = 'green';
-// });
-// for (const child of itemList.children) {
-//     child.style.backgroundColor = 'blue';
-// }
+// console.log(itemList.children[2]);
+// itemList.children[2].style.backgroundColor = 'yellow';
+
+// firstChild
+// console.log(itemList.firstChild);
+
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'First child';
+
+// lastChild
+// console.log(itemList.lastChild);
+
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Last child';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+// itemList.nextElementSibling.textContent = 'Hi!!!';
+// itemList.nextElementSibling.innerHTML = `<h1>${'Hello Earth!'}</h1>`;
+
+// previousSibling
+// console.log(itemList.previousSibling);
+
+// previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.textContent = 'Hello Item';
 
 // * Create Element 
 
 // let div = document.createElement('div');
-
 // div.id = 'myDivId';
 
-// Create text node
-// let divTxt = document.createTextNode('Hello, world!');
-// div.appendChild(divTxt);
+// Create text node and append to div
+// div.appendChild(document.createTextNode('Hello, world!'));
+// console.log(div.textContent);
 
 // let container = document.querySelector('.container #main');
 // let h2 = document.querySelector('#main h2');
+// let h2 = document.querySelector('#main .test');
 
-// console.log(div);
-// container.insertBefore(div, h2);
+// container.insertBefore(div, h2);    // first parameter: the node that you want to insert.
+                                    // second parameter: the node before which you want to insert the new node.
 
-// let changeMe = document.getElementById('myDivId');
-// console.log(changeMe);
-// changeMe.style.backgroundColor = 'blue';
+// let element = document.getElementById('myDivId');
+// console.log(element);
+// element.style.color = 'green';
 
 // * Part 3
 
